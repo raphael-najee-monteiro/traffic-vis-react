181846c96221e8f0a001d2f88a2feec64ca294ef
Raphaël Monteiro <monterap@students.zhaw.ch>
2024-07-25 02:09:30 +0200

    small changes

--- App.tsx
+++ App.tsx
@@ -1,201 +1,128 @@
-import React, {useState, useMemo} from 'react';
-import {createRoot} from 'react-dom/client';
-import {Map} from 'react-map-gl/maplibre';
+import React, { useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
 import DeckGL from '@deck.gl/react';
-import {GeoJsonLayer} from '@deck.gl/layers';
-import {scaleLinear, scaleThreshold} from 'd3-scale';
-import {CSVLoader} from '@loaders.gl/csv';
-import {load} from '@loaders.gl/core';
+import { GeoJsonLayer } from '@deck.gl/layers';
+import { Map } from 'react-map-gl/maplibre';
+import { load } from '@loaders.gl/core';
+import { CSVLoader } from '@loaders.gl/csv';
+import { Feature, Point, GeoJsonProperties } from 'geojson';
+import {MapViewState} from "deck.gl";
 
-import {Feature, LineString, MultiLineString} from 'geojson';
-import type {Color, PickingInfo, MapViewState} from '@deck.gl/core';
-
+// Define the URL to the nodes CSV file
 const DATA_URL = {
-  ACCIDENTS:
-      'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/highway/accidents.csv',
-  ROADS: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/highway/roads.json'
+    NODES: 'csv/Zurich_2024_nodes.csv' // Update this path
 };
 
-export const COLOR_SCALE = scaleThreshold<number, Color>()
-    .domain([0, 4, 8, 12, 20, 32, 52, 84, 136, 220])
-    .range([
-      [26, 152, 80],
-      [102, 189, 99],
-      [166, 217, 106],
-      [217, 239, 139],
-      [255, 255, 191],
-      [254, 224, 139],
-      [253, 174, 97],
-      [244, 109, 67],
-      [215, 48, 39],
-      [168, 0, 0]
-    ]);
-
-const WIDTH_SCALE = scaleLinear().clamp(true).domain([0, 200]).range([10, 2000]);
-
 const INITIAL_VIEW_STATE: MapViewState = {
-  latitude: 38,
-  longitude: -100,
-  zoom: 4,
-  minZoom: 2,
-  maxZoom: 8
+    latitude: 47.4,
+    longitude: 8.55,
+    zoom: 13,
+    minZoom: 1,
+    maxZoom: 20
 };
 
 const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
 
-type Accident = {
-  state: string;
-  type: string;
-  id: string;
-  year: number;
-  incidents: number;
-  fatalities: number;
+// Define the structure of Node data
+type Node = {
+    node_id: string;
+    lon: number;
+    lat: number;
+    type: string;
+    node_geom: string;
 };
 
-type RoadProperties = {
-  state: string;
-  type: string;
-  id: string;
-  name: string;
-  length: number;
-};
-
-type Road = Feature<LineString | MultiLineString, RoadProperties>;
-
-function getKey({state, type, id}: Accident | RoadProperties) {
-  return `${state}-${type}-${id}`;
-}
-
-function aggregateAccidents(accidents?: Accident[]) {
-  const incidents: {[year: number]: Record<string, number>} = {};
-  const fatalities: {[year: number]: Record<string, number>} = {};
-
-  if (accidents) {
-    for (const a of accidents) {
-      const r = (incidents[a.year] = incidents[a.year] || {});
-      const f = (fatalities[a.year] = fatalities[a.year] || {});
-      const key = getKey(a);
-      r[key] = a.incidents;
-      f[key] = a.fatalities;
-    }
-  }
-  return {incidents, fatalities};
+// Function to convert CSV data to GeoJSON format
+function convertToGeoJSON(nodes: Node[]): Feature<Point, GeoJsonProperties>[] {
+    return nodes.map(node => ({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [node.lon, node.lat]
+        },
+        properties: {
+            node_id: node.node_id,
+            type: node.type,
+            node_geom: node.node_geom
+        }
+    }));
 }
 
 function renderTooltip({
-                         fatalities,
-                         incidents,
-                         year,
-                         hoverInfo
+                           hoverInfo
                        }: {
-  fatalities: {[year: number]: Record<string, number>};
-  incidents: {[year: number]: Record<string, number>};
-  year: number;
-  hoverInfo?: PickingInfo<Road>;
+    hoverInfo?: any; // Adjust type based on your hoverInfo structure
 }) {
-  if (!hoverInfo?.object) {
-    return null;
-  }
+    if (!hoverInfo?.object) {
+        return null;
+    }
 
-  const {object, x, y} = hoverInfo;
-  const props = object.properties;
-  const key = getKey(props);
-  const f = fatalities[year][key];
-  const r = incidents[year][key];
+    const { object, x, y } = hoverInfo;
+    const { properties } = object;
+    const content = (
+        <div>
+            Node ID: <b>{properties.node_id}</b><br/>
+            Type: <b>{properties.type}</b><br/>
+            Geometry: <b>{properties.node_geom}</b>
+        </div>
+    );
 
-  const content = r ? (
-      <div>
-        <b>{f}</b> people died in <b>{r}</b> crashes on{' '}
-        {props.type === 'SR' ? props.state : props.type}-{props.id} in <b>{year}</b>
-      </div>
-  ) : (
-      <div>
-        no accidents recorded in <b>{year}</b>
-      </div>
-  );
-
-  return (
-      <div className="tooltip" style={{left: x, top: y}}>
-        <big>
-          {props.name} ({props.state})
-        </big>
-        {content}
-      </div>
-  );
+    return (
+        <div className="tooltip" style={{ left: x, top: y }}>
+            {content}
+        </div>
+    );
 }
 
-export default function App({
-                              roads = DATA_URL.ROADS,
-                              year,
-                              accidents,
-                              mapStyle = MAP_STYLE
-                            }: {
-  roads?: string | Road[];
-  accidents?: Accident[];
-  year?: number;
-  mapStyle?: string;
-}) {
-  const [hoverInfo, setHoverInfo] = useState<PickingInfo<Road>>();
-  const {incidents, fatalities} = useMemo(() => aggregateAccidents(accidents), [accidents]);
+function App() {
+    const [nodes, setNodes] = useState<Feature<Point, GeoJsonProperties>[]>([]);
+    const [hoverInfo, setHoverInfo] = useState<any>();
 
-  const layers = [
-    new GeoJsonLayer<RoadProperties>({
-      id: 'geojson',
-      data: roads,
-      lineWidthMinPixels: 0.5,
+    useEffect(() => {
+        // Load the nodes data from CSV
+        async function fetchData() {
+            const data = await load(DATA_URL.NODES, CSVLoader);
 
-      getLineColor: (f: Road) => {
-        if (!fatalities[year]) {
-          return [200, 200, 200];
+            // Ensure data is in the correct format and map to Node type
+            const nodesData = (data as unknown as Node[]).map(row => ({
+                node_id: row.node_id,
+                lon: parseFloat(String(row.lon)),
+                lat: parseFloat(String(row.lat)),
+                type: row.type,
+                node_geom: row.node_geom
+            }));
+
+            const geoJsonData = convertToGeoJSON(nodesData);
+            setNodes(geoJsonData);
         }
-        const key = getKey(f.properties);
-        const fatalitiesPer1KMile = ((fatalities[year][key] || 0) / f.properties.length) * 1000;
-        return COLOR_SCALE(fatalitiesPer1KMile);
-      },
-      getLineWidth: (f: Road) => {
-        if (!incidents[year]) {
-          return 10;
-        }
-        const key = getKey(f.properties);
-        const incidentsPer1KMile = ((incidents[year][key] || 0) / f.properties.length) * 1000;
-        return WIDTH_SCALE(incidentsPer1KMile);
-      },
+        fetchData();
+    }, []);
 
-      pickable: true,
-      onHover: setHoverInfo,
+    const layers = [
+        new GeoJsonLayer({
+            id: 'nodes-layer',
+            data: nodes,
+            pointRadiusMinPixels: 10,
+            getPointRadius: 10,
+            getFillColor: [255, 0, 0, 255],
+            pickable: true,
+            onHover: setHoverInfo
+        })
+    ];
 
-      updateTriggers: {
-        getLineColor: {year},
-        getLineWidth: {year}
-      },
-
-      transitions: {
-        getLineColor: 1000,
-        getLineWidth: 1000
-      }
-    })
-  ];
-
-  return (
-      <DeckGL
-          layers={layers}
-          pickingRadius={5}
-          initialViewState={INITIAL_VIEW_STATE}
-          controller={true}
-      >
-        <Map reuseMaps mapStyle={mapStyle} />
-
-        {renderTooltip({incidents, fatalities, year, hoverInfo})}
-      </DeckGL>
-  );
+    return (
+        <DeckGL
+            layers={layers}
+            initialViewState={INITIAL_VIEW_STATE}
+            controller={true}
+        >
+            <Map reuseMaps mapStyle={MAP_STYLE} />
+            {renderTooltip({ hoverInfo })}
+        </DeckGL>
+    );
 }
 
 export async function renderToDOM(container: HTMLDivElement) {
-  const root = createRoot(container);
-  root.render(<App />);
-
-  const accidents = (await load(DATA_URL.ACCIDENTS, CSVLoader)).data;
-
-  // @ts-ignore
-    root.render(<App accidents={accidents} year={accidents[0].year} />);
+    const root = createRoot(container);
+    root.render(<App />);
 }
